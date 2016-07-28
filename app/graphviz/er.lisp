0b4ba424424244c83dbc1213ec6247fd56ce9edd
mukaiizawa <mukaiizawa@yahoo.co.jp>
2016-07-28 10:29:33 +0900

    Add deftables

--- app/graphviz/er.lisp
+++ app/graphviz/er.lisp
@@ -2,10 +2,7 @@
 (load "graphviz")
 
 (set-attr! 'shape "record" *global-node-conf*)
-
-
-; (dot (make-graph :nodes (append (make-records '(("q_and_a" "q_and_a_no" "title")) '((color "grey")))
-;                                 (make-nodes '("a" "b" "c")))))
+(set-attr! 'style "vee" *global-edge-conf*)
 
 (defstruct table name columns)
 
@@ -23,16 +20,29 @@
                :columns (list ,@(mapcar (lambda (col)
                                           `(make-column
                                              :phisical-name ,(mkstr (first col))
-                                             ;; exclude phisical-name
-                                             ,@(flatten (mapcar (lambda (col-exclude-phisical-name)
-                                                                  (case (first col-exclude-phisical-name)
-                                                                    (:length (list (first col-exclude-phisical-name) (parse-int (mkstr (second col-exclude-phisical-name)))))
-                                                                    (:primarykey? col-exclude-phisical-name)
-                                                                    (:required? col-exclude-phisical-name)
-                                                                    (:foreignkey col-exclude-phisical-name)
-                                                                    (t (list (first col-exclude-phisical-name) (mkstr (second col-exclude-phisical-name))))))
-                                                                (group (rest col) 2)))))
+                                             ,@(rest col)))
                                         columns))))
 
-; (defmethod table->dot ((table table))
+(defmacro deftables (&rest tables)
+  `(list
+     ,@(mapcar (lambda (table) 
+                 `(deftable ,(first table)
+                    ,@(rest table)))
+               tables)))
+
+(defun tables->nodes (tables)
+  (make-records (mapcar (lambda (table)
+                          (cons (table-name table)
+                                (mapcar (lambda (column)
+                                          (column-phisical-name column))
+                                        (table-columns table))))
+                        tables)))
+
+(defun tables->edges (tables)
+  (let (acc)
+    (dolist (table tables)
+      (dolist (column (table-columns table))
+        (awhen (column-foreignkey column)
+          (push (list (table-name table) (first it)) acc))))
+    (make-edges acc)))
 
