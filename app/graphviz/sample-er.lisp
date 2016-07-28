7cdedcf7be4cb8c587dea7548838ab809f4dcad8
mukaiizawa <mukaiizawa@yahoo.co.jp>
2016-07-28 15:27:58 +0900

    Apply new er.lisp

--- app/graphviz/sample-er.lisp
+++ app/graphviz/sample-er.lisp
@@ -3,12 +3,14 @@
 
 (defparameter tables
   (deftables
-    (user
-      (user_id :logical-name "ユーザーID" :primarykey? t :type "varchar2" :required? t)
-      (user_name :logical-name "ユーザー名" :type "varchar2"))
-    (group
-      (group_id :logical-name "部署ID" :primarykey? t :foreignkey '(user user_id) :type "varchar2" :required? t)
-      (group_name :logical-name "部署名" :type "varchar2"))))
+    (user_tbl
+      "ユーザーマスタ"
+      (user_id "ユーザーID" :primarykey? t :type "varchar2" :required? t)
+      (user_name "ユーザー名" :type "varchar2"))
+    (group_tbl
+      "部署テーブル"
+      (group_id "部署ID" :primarykey? t :foreignkey '(user_tbl user_id) :type "varchar2" :required? t)
+      (group_name "部署名" :type "varchar2"))))
 
 (dot (make-graph :nodes (tables->nodes tables)
                  :edges (tables->edges tables))
