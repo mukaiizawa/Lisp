6d1477779bf9180b675564bc082716f4344034ac
mukaiizawa <mukaiizawa@yahoo.co.jp>
2016-08-04 10:54:10 +0900

    Update demo

--- web/demo.lisp
+++ web/demo.lisp
@@ -1,8 +1,5 @@
 
-(load "../lib/stdlib")
-(load "to-html")
-
-; (setq *html-indent* nil)
+(require "xml-manager" *module-xml-manager*)
 
 (with-html-output ()
   (:!DOCTYPE "html")
@@ -11,9 +8,10 @@
       (:meta ((charset "utf-8")))
       (:title "Page Title"))
     (:body
-      (:table ((border 0) (cellpadding 40))
+      (:table ((border 0) (cellpadding 10))
         (dotimes (i 5)
-        (:tr ((align "right"))
-          (dotimes (j 5)
-            (:td ((bgcolor (if (oddp (1+ j)) "pink" "green")))
-              (format nil "~@R" (* (1+ i) (1+ j )))))))))))
+          (:tr ((align "right"))
+            (dotimes (j 5)
+              (:td ((bgcolor (if (oddp (1+ j)) "#FF0000" "#FF00FF")))
+                (format nil "~@R" (+ (* 5 i) (1+ j)))))))))))
+
