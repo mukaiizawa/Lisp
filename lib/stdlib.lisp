ca3b0eab79dc359e31534e57c93673e692abcdab
mukaiizawa <mukaiizawa@yahoo.co.jp>
2016-08-14 08:20:35 +0900

    Delete stream->string

--- lib/stdlib.lisp
+++ lib/stdlib.lisp
@@ -912,14 +912,6 @@
 ;; => (B O M B)
 
 ;; }}}
-;; stream->string {{{
-
-(defun stream->string (stream)
-  (with-output-to-string (out)
-    (awhile (read-char stream nil nil)
-      (write-char it out))))
-
-;; }}}
 ;; string->list {{{
 
 (defun string->list (char str)
