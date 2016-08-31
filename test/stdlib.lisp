fa76568e7d241c6eb1b3cd1c584678ec68a644cd
mukaiizawa <mukaiizawa@yahoo.co.jp>
2016-08-31 12:42:49 +0900

    Add alambda test

--- test/stdlib.lisp
+++ test/stdlib.lisp
@@ -44,5 +44,26 @@ END
   )
 
 ;; }}}
+;; alambda {{{
+
+(test-all
+  ('alambda-fact
+   (funcall (alambda (n)
+              (if (= n 1)
+                1
+                (* n (self (1- n)))))
+            5)
+   120)
+  ('alambda-fib
+   (mapcar (alambda (n)
+             (if (or (= n 1) (= n 2))
+               1
+               (+ (self (- n 1))
+                  (self (- n 2)))))
+           '(1 2 3 4 5 6 7 8 9 10))
+   '(1 1 2 3 5 8 13 21 34 55))
+  )
+
+;; }}}
 
 
