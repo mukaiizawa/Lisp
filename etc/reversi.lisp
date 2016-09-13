9228c8aaa90c55f22c77ddd704ea3bd3c75e1472
mukaiizawa <mukaiizawa@yahoo.co.jp>
2016-09-13 13:52:37 +0900

    Update

--- etc/reversi.lisp
+++ etc/reversi.lisp
@@ -4,25 +4,35 @@
 
 (defparameter *width* 40)
 
+(defmacro draw-board ()
+  `(dorange (i 0 7)
+     (dorange (j 0 7)
+       (itemconfigure
+         canvas
+         (create-rectangle canvas
+                           (* i *width*)
+                           (* j *width*)
+                           (+ (* *width* i) *width*)
+                           (+ (* *width* j) *width*))
+         "fill" "green"))))
+
+(defmacro draw-oval (turn i j)
+  `(itemconfigure
+     canvas
+     (create-oval canvas 0 0 (* i *width*) (* j *width*))
+     "fill" (mkstr ,turn)))
+
 (with-ltk ()
   (bind *tk* "<Alt-q>" (alambda (event) (setf *exit-mainloop* t)))
   (let ((canvas (make-instance 'canvas  :width (* *width* 8) :height (* *width* 8)))
+        (turn 'black)
         (items))
     (pack canvas)
-    (dorange (i 0 7)
-      (dorange (j 0 7)
-        (push 
-          (create-rectangle canvas
-                            (* i *width*)
-                            (* j *width*)
-                            (+ (* *width* i) *width*)
-                            (+ (* *width* j) *width*))
-          items)
-        (itemconfigure
-          canvas
-          (first items)
-          "fill" "green")))
+    ; (draw-board)
     (bind canvas "<ButtonPress-1>"
-          (ilambda (event)
-            (itemdelete canvas (first items))))))
+          (lambda (event)
+            (let ((i (truncate (event-y event) *width*))
+                  (j (truncate (event-x event) *width*)))
+              (draw-oval 'black i j)
+              )))))
 
