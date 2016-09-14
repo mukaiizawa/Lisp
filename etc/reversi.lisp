0574986329bb414e541b23a1d8ae7054fdad3f20
mukaiizawa <mukaiizawa@yahoo.co.jp>
2016-09-14 12:32:06 +0900

    Update

--- etc/reversi.lisp
+++ etc/reversi.lisp
@@ -3,36 +3,47 @@
 (require "stdlib" *module-stdlib*)
 
 (defparameter *width* 40)
+(defparameter *board* (make-array '(8 8) :initial-element 'none))
 
 (defmacro draw-board ()
-  `(dorange (i 0 7)
-     (dorange (j 0 7)
-       (itemconfigure
-         canvas
-         (create-rectangle canvas
-                           (* i *width*)
-                           (* j *width*)
-                           (+ (* *width* i) *width*)
-                           (+ (* *width* j) *width*))
-         "fill" "green"))))
+  `(progn
+     (dorange (i 0 7)
+       (dorange (j 0 7)
+         (itemconfigure
+           canvas
+           (create-rectangle canvas
+                             (* i *width*)
+                             (* j *width*)
+                             (+ (* *width* i) *width*)
+                             (+ (* *width* j) *width*))
+           "fill" "green")))
+     (put-oval 'black 3 3)
+     (put-oval 'black 4 4)
+     (put-oval 'white 3 4)
+     (put-oval 'white 4 3)))
 
-(defmacro draw-oval (turn i j)
-  `(itemconfigure
-     canvas
-     (create-oval canvas 0 0 (* i *width*) (* j *width*))
-     "fill" (mkstr ,turn)))
+(defmacro put-oval (turn i j)
+  `(itemconfigure canvas 
+                  (create-oval canvas
+                               (* ,i *width*)
+                               (* ,j *width*)
+                               (+ (* ,i *width*) *width*)
+                               (+ (* ,j *width*) *width*))
+                  "fill" (mkstr ,turn)))
+
+(defun toggle (x y1 y2)
+  (if (eq x y1) y2 y1))
 
 (with-ltk ()
-  (bind *tk* "<Alt-q>" (alambda (event) (setf *exit-mainloop* t)))
-  (let ((canvas (make-instance 'canvas  :width (* *width* 8) :height (* *width* 8)))
-        (turn 'black)
-        (items))
-    (pack canvas)
-    ; (draw-board)
+  (bind *tk* "<Alt-q>" (ilambda (event) (setf *exit-mainloop* t)))
+  (let ((canvas (pack (make-instance 'canvas  :width (* *width* 8) :height (* *width* 8))))
+        (turn 'black))
+    (draw-board)
     (bind canvas "<ButtonPress-1>"
           (lambda (event)
-            (let ((i (truncate (event-y event) *width*))
-                  (j (truncate (event-x event) *width*)))
-              (draw-oval 'black i j)
+            (let ((i (truncate (event-x event) *width*))
+                  (j (truncate (event-y event) *width*)))
+              (put-oval turn i j)
+              (setq turn (toggle turn 'black 'white))
               )))))
 
