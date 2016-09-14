2aaced0d3b0ed991c2a4a767721d49b5ca0bad22
mukaiizawa <mukaiizawa@yahoo.co.jp>
2016-09-14 15:24:23 +0900

    Rename and improved

--- etc/reversi.lisp
+++ etc/reversi.lisp
@@ -17,19 +17,22 @@
                              (+ (* *width* i) *width*)
                              (+ (* *width* j) *width*))
            "fill" "green")))
-     (put-oval 'black 3 3)
-     (put-oval 'black 4 4)
-     (put-oval 'white 3 4)
-     (put-oval 'white 4 3)))
+     (draw-disc 'black 3 3 4 4)
+     (draw-disc 'white 3 4 4 3)))
 
-(defmacro put-oval (turn i j)
-  `(itemconfigure canvas 
-                  (create-oval canvas
-                               (* ,i *width*)
-                               (* ,j *width*)
-                               (+ (* ,i *width*) *width*)
-                               (+ (* ,j *width*) *width*))
-                  "fill" (mkstr ,turn)))
+(defmacro draw-disc (turn &rest cordinates)
+  `(progn
+     ,@(mapcar (lambda (cordinate)
+                 (let ((i (first cordinate))
+                       (j (second cordinate)))
+                   `(itemconfigure canvas 
+                                   (create-oval canvas
+                                                (* ,i *width*)
+                                                (* ,j *width*)
+                                                (+ (* ,i *width*) *width*)
+                                                (+ (* ,j *width*) *width*))
+                                   "fill" (mkstr ,turn))))
+               (group cordinates 2))))
 
 (defun toggle (x y1 y2)
   (if (eq x y1) y2 y1))
@@ -38,12 +41,12 @@
   (bind *tk* "<Alt-q>" (ilambda (event) (setf *exit-mainloop* t)))
   (let ((canvas (pack (make-instance 'canvas  :width (* *width* 8) :height (* *width* 8))))
         (turn 'black))
-    (draw-board)
+    ; (draw-board)
     (bind canvas "<ButtonPress-1>"
           (lambda (event)
             (let ((i (truncate (event-x event) *width*))
                   (j (truncate (event-y event) *width*)))
-              (put-oval turn i j)
+              (draw-disc turn i j)
               (setq turn (toggle turn 'black 'white))
               )))))
 
