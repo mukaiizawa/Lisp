c1a7150508189c491f09af95212298d1179cfdb9
mukaiizawa <mukaiizawa@yahoo.co.jp>
2016-09-21 14:59:14 +0900

    Update

--- lib/csv-reader.lisp
+++ lib/csv-reader.lisp
@@ -2,3 +2,36 @@
 (require "ahead-reader" *module-ahead-reader*)
 (provide "csv-reader")
 
+(defstruct (csv-reader (:include ahead-reader))
+  (lines nil :type list)
+  (wrap? t :type boolean)
+  (wrapper #\" :type character)
+  (segment-character #\, :type character))
+
+(defstruct csv-writer
+  (lines nil :type list)
+  (wrap? t :type boolean)
+  (wrapper #\")
+  (segment-character #\, :type character))
+
+;; todo
+(defmacro with-csv-reader ((reader &optional (stream *standard-input*) (segment-character #\,) &body body)
+  `(let* ((,reader (make-csv-reader :stream ,stream :)))
+     ,@body))
+
+(defmacro with-string-ahead-reader ((reader str) &body body)
+  (with-gensyms (in)
+    `(with-input-from-string (,in ,str)
+       (with-ahead-reader (,reader ,in)
+         ,@body))))
+
+(defmacro with-open-ahead-reader ((reader pathname &key (if-does-not-exist :error)) &body body)
+  (with-gensyms (stream)
+    `(let (,stream)
+       (unwind-protect
+         (progn 
+           (setq ,stream (open ,pathname :direction :input :if-does-not-exist ,if-does-not-exist))
+           (with-ahead-reader (,reader ,stream)
+             ,@body))
+         (when (open-stream? ,stream)
+           (close ,stream))))))
