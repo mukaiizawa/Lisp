8c32348f0b97ddee7b7431c924d1882f474fc173
mukaiizawa <mukaiizawa@yahoo.co.jp>
2016-09-23 17:24:36 +0900

    Add queing-theory.lisp

---
+++ etc/queueing-theory.lisp
@@ -0,0 +1,41 @@
+
+(require "stdlib" *module-stdlib*)
+(require "math" *module-math*)
+
+;; queueing-theory
+;; A/B/C
+;; A := distribution of arrival time
+;; B := distribution of service time
+;; C := the number of parallel servers
+
+;; Wq := mean waiting time
+;; c := number of server
+;; mu  := the average service rate of a single service
+;; Wt (<=>1/mu)  := the average service time of a single task
+;; rho := traffic intensity
+
+;; Wq := p / c*mu(1 - rho) = c*(1-rho)*Wt
+;; p := (c^c)*(rho^c)p0/c!(1-rho)
+;; p0 := [Sum_c-1_k=0{ (c^k)(rho^k)/k! + (c^c)(rho^c)/c!(1 - rho) }]^-1
+
+(defun p0 (c rho)
+  (/ 1
+     (sigma 0 (1- c)
+            (lambda (k)
+              (+
+                (/ (* (expt c k) (expt rho k))
+                   (fact k))
+                (/ (* (expt c c) (expt rho c))
+                   (* (fact c) (- 1 rho))))))))
+
+(defun p (c rho)
+  (/ (* (expt c c) (expt rho c) (p0 c rho))
+     (* (fact c) (- 1 rho))))
+
+(defun a (c rho)
+  (/ (p c rho)
+     (* c (- 1 rho))))
+
+(for (i 0 (< i 0.95) :step 0.01)
+  (echo i #\tab (a 2 i)))
+
