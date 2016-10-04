deba0e486dfa67b37c8b28994c237f11b0c1398d
mukaiizawa <mukaiizawa@yahoo.co.jp>
2016-10-04 13:40:12 +0900

    Rename

--- lib/coordinate-manager.lisp
+++ lib/coordinate-manager.lisp
@@ -1,42 +1,42 @@
 
 (require "stdlib" *module-stdlib*)
 
-(defstruct cordinate
+(defstruct coordinate
   (x 0 :type number)
   (y 0 :type number)
   (z 0 :type number))
 
-(defmacro with-cordinates ((cordinate &rest cordinates) &body body)
+(defmacro with-coordinates ((coordinate &rest coordinates) &body body)
   `(ilet* (,@(do* ((acc)
                    (counter 1 (1+ counter))
-                   (cordinates (cons cordinate cordinates) (rest cordinates))
-                   (cordinate (first cordinates) (first cordinates)))
-               ((null cordinate) (nreverse acc))
-               (push `(,(mksym 'r counter) ,cordinate) acc)
-               (push `(,(mksym 'x counter) (cordinate-x ,cordinate)) acc)
-               (push `(,(mksym 'y counter) (cordinate-y ,cordinate)) acc)
-               (push `(,(mksym 'z counter) (cordinate-z ,cordinate)) acc)))
+                   (coordinates (cons coordinate coordinates) (rest coordinates))
+                   (coordinate (first coordinates) (first coordinates)))
+               ((null coordinate) (nreverse acc))
+               (push `(,(mksym 'r counter) ,coordinate) acc)
+               (push `(,(mksym 'x counter) (coordinate-x ,coordinate)) acc)
+               (push `(,(mksym 'y counter) (coordinate-y ,coordinate)) acc)
+               (push `(,(mksym 'z counter) (coordinate-z ,coordinate)) acc)))
           ,@body))
 
 ;; make-vector {{{
 
 (defun make-vector (&optional (x 0) (y 0) (z 0))
-  (make-cordinate :x x
+  (make-coordinate :x x
                   :y y
                   :z z))
 
 ;; }}}
 ;; make-vector-list {{{
 
-(defmacro make-vector-list (cordinates)
-  `(mapcar (lambda (cordinate)
-             (list->vector cordinate))
-           ,cordinates))
+(defmacro make-vector-list (coordinates)
+  `(mapcar (lambda (coordinate)
+             (list->vector coordinate))
+           ,coordinates))
 
 ;; }}} ;; vector->list {{{
 
-(defmethod vector->list ((r1 cordinate))
-  (with-cordinates (r1)
+(defmethod vector->list ((r1 coordinate))
+  (with-coordinates (r1)
     (list x1 y1 z1)))
 
 ;; }}}
@@ -50,17 +50,17 @@
 ;; }}}
 ;; vector+ {{{
 
-(defmethod vector+ ((r1 cordinate) (r2 cordinate))
-  (with-cordinates (r1 r2)
-    (make-cordinate :x (+ x1 x2)
+(defmethod vector+ ((r1 coordinate) (r2 coordinate))
+  (with-coordinates (r1 r2)
+    (make-coordinate :x (+ x1 x2)
                     :y (+ y1 y2)
                     :z (+ z1 z2))))
 
 ;; }}}
 ;; vector-rotate {{{
 
-(defmethod vector-rotate ((r1 cordinate) (radian number))
-  (with-cordinates (r1)
+(defmethod vector-rotate ((r1 coordinate) (radian number))
+  (with-coordinates (r1)
     (make-vector (- (* (cos radian) x1)
                     (* (sin radian) y1))
                  (+ (* (sin radian) x1)
@@ -69,7 +69,7 @@
 ;; }}}
 ;; vector-norm {{{
 
-(defmethod vector-norm ((r1 cordinate))
+(defmethod vector-norm ((r1 coordinate))
   (sqrt (apply #'+
                (mapcar (lambda (x)
                          (expt x 2))
@@ -78,8 +78,8 @@
 ;; }}}
 ;; vector-normalize {{{
 
-(defmethod vector-normalize ((r1 cordinate))
-  (with-cordinates (r1)
+(defmethod vector-normalize ((r1 coordinate))
+  (with-coordinates (r1)
     (let1 (norm (vector-norm r1))
       (make-vector (/ x1 norm) (/ y1 norm) (/ z1 norm)))))
 
