cf6fac60dde35039e24945f3aabd7e464ad4582f
mukaiizawa <mukaiizawa@yahoo.co.jp>
2016-11-02 09:33:43 +0900

    Change character

--- etc/queueing-theory.lisp
+++ etc/queueing-theory.lisp
@@ -12,38 +12,38 @@
 ;; Wq := mean waiting time
 ;; Wt (<=>1/mu)  := the average service time of a single task
 ;; c := number of server
-;; mu  := the average service rate of a single service
-;; rho := traffic intensity
+;; μ  := the average service rate of a single service
+;; ρ := traffic intensity
 
 ;; Wr := Wt + Wq
 ;; Wq := (p/c*(1-rho))Wt
-;; p := ((c*rho)^c * p0) / c!(1 - rho)
-;; p0 := [Sigma_c-1_n=0{ (c*rho)^n / n! } + (c*rho)^c / c!(1 - rho) ]^-1
+;; p := ((c*rho)^c * p0) / c!(1 - ρ)
+;; p0 := [Sigma_c-1_n=0{ (c*rho)^n / n! } + (c*rho)^c / c!(1 - ρ) ]^-1
 
-(defparameter c 2)
-(defparameter lambda 7.778)
+(defparameter C 2)
+(defparameter λ 7.778)
 
-(defmacro rho ()
-  `(/ (* lambda wt)
-      c))
+(defmacro ρ ()
+  `(/ (* λ Wt)
+      C))
 
 (defmacro p0 ()
   `(/ 1
       (+
-        (sigma 0 (1- c)
+        (sigma 0 (1- C)
                (lambda (n)
-                 (/ (expt (* c (rho)) n)
+                 (/ (expt (* C (ρ)) n)
                     (fact n))))
-        (/ (expt (* c (rho)) c)
-           (* (fact c) (- 1 (rho)))))))
+        (/ (expt (* C (ρ)) C)
+           (* (fact C) (- 1 (ρ)))))))
 
 (defmacro p ()
-  `(/ (* (expt (* c (rho)) c) (p0))
-      (* (fact c) (- 1 (rho)))))
+  `(/ (* (expt (* C (ρ)) C) (p0))
+      (* (fact C) (- 1 (ρ)))))
 
 (defmacro Wq ()
   `(* (/ (p)
-         (* c (- 1 (rho))))
+         (* C (- 1 (ρ))))
       Wt))
 
 (defmacro Wr ()
