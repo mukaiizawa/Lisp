61105794eda794e18e87e98dc11702d1415d82ff
mukaiizawa <mukaiizawa@yahoo.co.jp>
2016-12-14 11:33:35 +0900

    Fix bug

--- etc/life.lisp
+++ etc/life.lisp
@@ -31,12 +31,11 @@
          (let (
                ,@(remove nil
                          (list
-                           `(,point (@ (mod ,g (width ,matrix)) (truncate ,g (height ,matrix))))
+                           `(,point (@ (mod ,g (width ,matrix)) (truncate ,g (width ,matrix))))
                            (when value `(,value (aref (matrix-contents ,matrix) ,g))))
                          :key #'first))
            ,@body))
        ,matrix)))
-; クロージャー前提となるがもちろん、defmethodで（冗長な記述ではあるが）同等の機能は提供できる。
 ; (defmethod doMatrix ((m matrix) (fn function))
 ;   (dotimes (i (* (width m) (height m)))
 ;     (funcall fn (@ (mod i (width m)) (truncate i (height m))) (aref (matrix-contents m) i)))
@@ -56,14 +55,14 @@
   (put (put stream obj) #\Newline))
 
 ;; life game main
-(defvar n 10)
+(defvar n 3)
 (defvar board (init (make-matrix) (@ n n)))
 (defvar neighbors (init (make-matrix) (@ n n)))
 (defun setNeighbors (neighbors)
   (doMatrix (neighbors p)
     (putAt neighbors p
-           (count-if (lambda (p2)
-                       (and (inside? board p2) (at board p2)))
+           (count-if (lambda (q)
+                       (and (inside? board q) (at board q)))
                      (apply #'append
                             (loop for i from -1 to 1
                                   collect (loop for j from -1 to 1 when (not (= i j 0))
@@ -75,24 +74,15 @@
 (defun showBoard (board)
   (doMatrix (board p v)
     (funcall (if (= (x p) (1- (width board))) #'putLn #'put) Out (if v #\# #\Space))))
-(defun setup ()
+(defun setup (board)
   (doMatrix (board p)
     (putAt board p (evenp (random 1000 (make-random-state t))))))
 (defun main ()
-  (setup)
-  (dotimes (i 2)
+  (setup board)
+  (dotimes (i 100)
+    #+clisp(shell "clear")    ; depend on clisp
     (showBoard (nextGeneration (setNeighbors neighbors)))
-    ; (format t "~C[2J" #\Esc)
-    ; (sleep 1))
-  ))
+    (sleep 1)))
 
 (main)
-
-; (defun sh (cmd)
-;    #+clisp (shell cmd)
-;    #+ecl (si:system cmd)
-;    #+sbcl (sb-ext:run-program "/bin/sh" (list "-c" cmd) :input nil :output *standard-output*)
-;    #+clozure (ccl:run-program "/bin/sh" (list "-c" cmd) :input nil :output *standard-output*))
-; (sh "clear")
-
 
