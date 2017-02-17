e91bc4839ead99500f7ca506f800fa1f56ae1888
mukaiizawa <mukaiizawa@yahoo.co.jp>
2017-02-17 13:06:42 +0900

    Update

--- etc/Puyo-Puyo.lisp
+++ etc/Puyo-Puyo.lisp
@@ -20,198 +20,184 @@
 (defstruct player
   canvas canvas-next puyos curr-puyos next-puyos)
 
-(defmacro with-player ((num) &body body)
-  `(ilet* ((player (symbol-value (mksym 'player ,num)))
-           (canvas (player-canvas player))
-           (canvas-next (player-canvas-next player))
-           (puyos (player-puyos player))
-           (curr-puyos (player-curr-puyos player))
-           (next-puyos (player-next-puyos player)))
-     ,@body))
-
 ;; cutting-puyo {{{
 
-(defun cutting-puyo (num)
-  (with-player (num)
-    (dolist (puyo curr-puyos)
-      (while (puttable? num (vector+ (puyo-point puyo) +vector-top+))
-        (with-coordinates ((vector+ (puyo-point puyo) +vector-top+))
-          (setf (puyo-point puyo) r1)
-          (itemmove canvas
-                    (puyo-id puyo)
-                    0
-                    (* y1 cell-size)))))))
+ (defun cutting-puyo (player) (print player))
+; (defun cutting-puyo (player)
+;   (dolist (puyo curr-puyos)
+;     (while (puttable? player (vector+ (puyo-point puyo) +vector-top+))
+;       (with-coordinates ((vector+ (puyo-point puyo) +vector-top+))
+;         (setf (puyo-point puyo) r1)
+;         (itemmove canvas
+;                   (puyo-id puyo)
+;                   0
+;                   (* y1 cell-size))))))
 
 ;; }}}
 ;; move-bottom {{{
 
-(defun move-bottom (num)
-  (while (movable? num +vector-top+)
-    (move-puyos num +vector-top+)))
+(defmethod move-bottom ((player player))
+  (while (movable? player +vector-top+)
+    (move-puyos player +vector-top+)))
 
 ;; }}}
 ;; puttable? {{{
 
-(defun puttable? (num points)
-  (with-player (num)
-    (every (lambda (point)
-             (and (in-canvas? point)
-                  (not
-                    (some (lambda (puyo)
-                            (vector= (puyo-point puyo) point))
-                          puyos))))
-           (mklist points))))
+(defmethod puttable? ((player player) points)
+  (every (lambda (point)
+           (and (in-canvas? point)
+                (not
+                  (some (lambda (puyo)
+                          (vector= (puyo-point puyo) point))
+                        (player-puyos player)))))
+         (mklist points)))
 
 ;; }}}
 ;; in-canvas? {{{
 
-(defun in-canvas? (p)
-  (with-coordinates (p)
+(defmethod in-canvas? ((point coordinate))
+  (with-coordinates (point)
     (and (<= 0 x1) (< x1 width)
          (<= 0 y1) (< y1 height))))
 
 ;; }}}
 ;; get-rotated-outer-puyo-point {{{
 
-(defun get-rotated-outer-puyo-point (num rot-dir)
-  (with-player (num)
-    (let* ((axis-puyo (first curr-puyos))
-           (outer-puyo (second curr-puyos))
-           (vector-axis (puyo-point axis-puyo))
-           (vector-outer (copy-coordinate (puyo-point outer-puyo))))
-      (vector+ vector-axis
-               (vector-rotate (vector- (puyo-point outer-puyo)
-                                       vector-outer)
-                              (if (eq rot-dir 'right)
-                                (/ pi 2)
-                                (- (/ pi 2))))))))
+(defmethod get-rotated-outer-puyo-point ((player player) (rot-dir symbol))
+  (let* ((axis-puyo (first (player-curr-puyos player)))
+         (outer-puyo (second (player-next-puyos player)))
+         (vector-axis (puyo-point axis-puyo))
+         (vector-outer (copy-coordinate (puyo-point outer-puyo))))
+    (vector+ vector-axis
+             (vector-rotate (vector- (puyo-point outer-puyo)
+                                     vector-outer)
+                            (if (eq rot-dir 'right)
+                              (/ pi 2)
+                              (- (/ pi 2)))))))
 
 ;; }}}
 ;; rotate-puyos {{{
 
-(defun rotate-puyos (num rot-dir)
-  (with-player (num)
-    (let* ((outer-puyo (second curr-puyos))
-           (vector-outer (copy-coordinate (puyo-point outer-puyo))))
-      (setf (puyo-point outer-puyo) (get-rotated-outer-puyo-point num rot-dir))
-      (with-coordinates ((vector- (puyo-point outer-puyo) vector-outer))
-        (itemmove canvas
-                  (puyo-id outer-puyo)
-                  (* x1 cell-size)
-                  (* y1 cell-size))))))
+(defmethod rotate-puyos ((player player) (rot-dir symbol))
+  (let* ((outer-puyo (second (player-curr-puyos player)))
+         (vector-outer (copy-coordinate (puyo-point outer-puyo))))
+    (setf (puyo-point
+            (second (player-curr-puyos player))) (get-rotated-outer-puyo-point player rot-dir))
+    (with-coordinates ((vector- (puyo-point
+                                  (second (player-curr-puyos player)))
+                                vector-outer))
+      (itemmove (player-canvas player)
+                (puyo-id (second (player-curr-puyos player)))
+                (* x1 cell-size)
+                (* y1 cell-size)))))
 
 ;; }}}
 ;; rotatable? {{{
 
-(defun rotatable? (num rot-dir)
-  (with-player (num)
-    (puttable? num (get-rotated-outer-puyo-point num rot-dir))))
+(defmethod rotatable? ((player player) (rot-dir symbol))
+  (puttable? player (get-rotated-outer-puyo-point player rot-dir)))
 
 ;; }}}
 ;; try-rotate {{{
 
-(defun try-rotate (num rot-dir)
-  (with-player (num)
-    (if (rotatable? num rot-dir)
-      (rotate-puyos num rot-dir))))
+(defmethod try-rotate ((player player) (rot-dir symbol))
+  (if (rotatable? player rot-dir)
+    (rotate-puyos player rot-dir)))
 
 ;; }}}
 ;; move-puyos {{{
 
-(defun move-puyos (num dir)
-  (with-player (num)
-    (with-coordinates (dir)
-      (dolist (puyo curr-puyos)
-        (setf (puyo-point puyo) (vector+ (puyo-point puyo) dir))
-        (itemmove canvas
-                  (puyo-id puyo)
-                  (* x1 cell-size)
-                  (* y1 cell-size))))))
+(defmethod move-puyos ((player player) (dir coordinate))
+  (with-coordinates (dir)
+    (dotimes (i 2)
+      (asetf (puyo-point (nth i (player-curr-puyos player))) (vector+ it dir))
+      (itemmove (player-canvas player)
+                (puyo-id (nth i (player-curr-puyos player)))
+                (* x1 cell-size)
+                (* y1 cell-size)))))
 
 ;; }}}
 ;; movable? {{{
 
-(defun movable? (num dir)
-  (with-player (num)
-    (puttable? num (mapcar (lambda (puyo)
-                             (vector+ dir (puyo-point puyo)))
-                           curr-puyos))))
+(defmethod movable? ((player player) (dir coordinate))
+  (puttable? player (mapcar (lambda (puyo)
+                              (vector+ dir (puyo-point puyo)))
+                            (player-curr-puyos player))))
 
 ;; }}}
 ;; try-move {{{
 
-(defun try-move (num dir)
-  (with-player (num)
-    (if (movable? num dir)
-      (move-puyos num dir)
-      (when (vector= dir +vector-top+)
-        (cutting-puyo num)
-        (setf puyos (append puyos curr-puyos))
-        (put-next-puyos num)))))
+(defmethod try-move ((player player) (dir coordinate))
+  (if (movable? player dir)
+    (move-puyos player dir)
+    (when (vector= dir +vector-top+)
+      (cutting-puyo player)
+      (asetf (player-puyos player) (append it (player-curr-puyos player)))
+      (put-next-puyos player))))
 
 ;; }}}
 ;; put-puyo {{{
 
-(defmethod put-puyo (num canvas (puyo puyo) (point coordinate))
-  (with-player (num)
-    (with-coordinates (point)
-      (setf (puyo-point puyo) point
-            (puyo-id puyo) (create-oval canvas
-                                        (* x1 cell-size)
-                                        (* y1 cell-size)
-                                        (+ (* cell-size x1) cell-size)
-                                        (+ (* cell-size y1) cell-size)))
-      (itemconfigure canvas
-                     (puyo-id puyo)
-                     "fill"
-                     (puyo-color puyo))
-      (values))))
+(defmethod put-puyo ((player player) (canvas-kind symbol) (puyo puyo) (point coordinate))
+  (with-coordinates (point)
+    (setf (puyo-point puyo) point
+          (puyo-id puyo) (create-oval (if (eq canvas-kind 'canvas)
+                                        (player-canvas player)
+                                        (player-canvas-next player))
+                                      (* x1 cell-size)
+                                      (* y1 cell-size)
+                                      (+ (* cell-size x1) cell-size)
+                                      (+ (* cell-size y1) cell-size)))
+    (itemconfigure (player-canvas player)
+                   (puyo-id puyo)
+                   "fill"
+                   (puyo-color puyo))
+    puyo))
 
 ;; }}}
 ;; put-next-puyos {{{
 
-(defun put-next-puyos (num)
-  (with-player (num)
-    (setf curr-puyos (loop for i from 0 to 1 collect
-                           (let ((puyo (nth i next-puyos)))
-                             (itemdelete canvas-next (puyo-id puyo))
-                             (put-puyo num canvas puyo (make-vector 2 i))
-                             puyo))
-          next-puyos (create-next-puyos num))))
+(defmethod put-next-puyos ((player player))
+  (setf (player-curr-puyos player) (loop for i from 0 to 1 collect
+                                         (let1 (puyo (nth i (player-next-puyos player)))
+                                           (itemdelete (player-canvas-next player) (puyo-id puyo))
+                                           (put-puyo player 'canvas puyo (make-vector 2 i))))
+        (player-next-puyos player) (create-next-puyos player)))
 
 ;; }}}
 ;; create-next-puyos {{{
 
-(defun create-next-puyos (num)
-  (with-player (num)
-    (loop for i from 0 to 1 collect
-          (let ((puyo (copy-puyo (nth (random (length puyo-origin) (make-random-state t))
-                                      puyo-origin))))
-            (put-puyo num canvas-next puyo (make-vector 0 i))
-            puyo))))
+(defmethod create-next-puyos ((player player))
+  (loop for i from 0 to 1 collect
+        (let1 (puyo (copy-puyo (nth (random (length puyo-origin) (make-random-state t))
+                                    puyo-origin)))
+          (put-puyo player 'canvas-next puyo (make-vector 0 i)))))
 
 ;; }}}
-;; init-canvas {{{
+;; init-player {{{
 
-(defmethod init-canvas (canvas)
+(defmethod init-player ((player player))
   (let* ((canvas-width (* cell-size width))
          (canvas-height (* cell-size height)))
-    (itemconfigure canvas
-                   (create-rectangle canvas 0 0 canvas-width canvas-height)
+    (itemconfigure (player-canvas player)
+                   (create-rectangle (player-canvas player)
+                                     0 0 canvas-width canvas-height)
                    "fill" "#c0c0c0")
     (dorange (x 0 width)
       (let1 (dx (* x cell-size))
-        (create-line canvas (list dx 0 dx canvas-height))))
+        (create-line (player-canvas player)
+                     (list dx 0 dx canvas-height))))
     (dorange (y 0 height)
       (let1 (dy (* y cell-size))
-        (create-line canvas (list 0 dy canvas-width dy))))))
+        (create-line (player-canvas player)
+                     (list 0 dy canvas-width dy))))))
 
 ;; }}}
 ;; init {{{
 
 (defun init ()
   (setf cell-size 30
-        drawing-interval 500
+        drawing-interval 2000
         end-game? nil
         frame (pack (make-instance 'frame))
         text-area (pack
@@ -231,17 +217,18 @@
                                               :height (* cell-size 2))
                                  :side :left)
                   :puyos nil
-                  :curr-puyos nil
-                  :next-puyos (create-next-puyos 1))
+                  :next-puyos nil
+                  :curr-puyos nil)
         puyo-origin (list
-                (make-puyo :sym 'red :color "#ff0000")
-                (make-puyo :sym 'green :color "#00ff00")
-                (make-puyo :sym 'blue :color "#0000ff")
-                (make-puyo :sym 'purple :color "#ff00ff")))
-  (init-canvas (player-canvas player1))
-  (put-next-puyos 1)
-  ; (put-next-puyos 2)
-  )
+                      (make-puyo :sym 'red :color "#ff0000")
+                      (make-puyo :sym 'green :color "#00ff00")
+                      (make-puyo :sym 'blue :color "#0000ff")
+                      (make-puyo :sym 'purple :color "#ff00ff")))
+  (init-player player1)
+  (setf (player-next-puyos player1) (create-next-puyos player1))
+  (print player1)
+  (put-next-puyos player1)
+  (print player1))
 
 ;; }}}
 ;; bind-key {{{
@@ -258,10 +245,10 @@
 (defmacro bind-keys ()
   `(progn
      (bind *tk* "<Control-c>" (ilambda (event) (setf *exit-mainloop* t)))
-     (bind-key #\h (try-move 1 +vector-left+))
-     (bind-key #\j (move-bottom 1))
-     (bind-key #\k (try-rotate 1 'right))
-     (bind-key #\l (try-move 1 +vector-right+))))
+     (bind-key #\h (try-move player1 +vector-left+))
+     (bind-key #\j (move-bottom player1))
+     (bind-key #\k (try-rotate player1 'right))
+     (bind-key #\l (try-move player1 +vector-right+))))
 
 ;; }}}
 ;; main {{{
@@ -270,8 +257,8 @@
   (after-time drawing-interval
               (lambda ()
                 (unless end-game?
-                  (try-move 1 +vector-top+)
-                  ; (try-move 2 +vector-top+)
+                  (try-move player1 +vector-top+)
+                  ; (try-move player2 +vector-top+)
                   (main)))))
 
 ;; }}}
