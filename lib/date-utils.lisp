77ad7581987b9885c21df0f58cda0b5c3b595a99
mukaiizawa <mukaiizawa@yahoo.co.jp>
2017-08-07 11:32:31 +0900

    Fix bug -- date-time construct with arguments

--- lib/date-utils.lisp
+++ lib/date-utils.lisp
@@ -1,4 +1,5 @@
 (require :stdlib *module-stdlib*)
+(provide :date-utils)
 
 (defstruct date-time
   utc
@@ -10,29 +11,35 @@
   sec
   day-of-week)
 
-(defun make-date-time (&key (_year 0) (_month 0) (_date 0))
-  (multiple-value-bind
-    (sec minute hour date month year day-of-week daylight-p zone)
-    (decode-universal-time (get-universal-time) -9)
-    (declare (ignore daylight-p zone))
-    (let ((dt (make-instance 'date-time
-                             :utc nil
-                             :year (if (= _year 0) year _year)
-                             :month (if (= _month 0) month _month)
-                             :date (if (= _date 0) date _date)
-                             :hour hour
-                             :minute minute
-                             :sec sec
-                             :day-of-week (mod (1+ day-of-week) 7))))
-      (setf (date-time-utc dt)
-            (encode-universal-time (sec dt)
-                                   (minute dt)
-                                   (hour dt)
-                                   (date dt)
-                                   (month dt)
-                                   (year dt)
-                                   -9))
-      dt)))
+(defun make-date-time (&optional year month date)
+  (let ((default? (every #'null (list year month date))))
+    (when (and (not default?) (some #'null (list year month date)))
+      (error "make-date-time: illegal arguments."))
+    (multiple-value-bind
+      (sec minute hour _date _month _year day-of-week daylight-p zone)
+      (decode-universal-time (get-universal-time) -9)
+      (declare (ignore daylight-p zone day-of-week))
+      (let* ((dt (make-instance 'date-time
+                                :year (if default? _year year)
+                                :month (if default? _month month)
+                                :date (if default? _date date)
+                                :hour (if default? hour 0)
+                                :minute (if default? minute 0)
+                                :sec (if default? sec 0)))
+             (utc (encode-universal-time (sec dt)
+                                         (minute dt)
+                                         (hour dt)
+                                         (date dt)
+                                         (month dt)
+                                         (year dt)
+                                         -9))
+             (day-of-week (mod (1+ (nth 6
+                                        (multiple-value-list
+                                          (decode-universal-time utc -9))))
+                               7)))
+        (setf (date-time-utc dt) utc
+              (date-time-day-of-week dt) day-of-week)
+        dt))))
 
 (defmethod year ((dt date-time))
   (date-time-year dt))
