293600860b2b4d25a50b7c9f82c9410afa699b75
mukaiizawa <mukaiizawa@yahoo.co.jp>
2017-08-08 09:12:30 +0900

    Upgrade constructor

--- lib/date-utils.lisp
+++ lib/date-utils.lisp
@@ -11,35 +11,29 @@
   sec
   day-of-week)
 
-(defun make-date-time (&optional year month date)
-  (let ((default? (every #'null (list year month date))))
-    (when (and (not default?) (some #'null (list year month date)))
-      (error "make-date-time: illegal arguments."))
+(defmethod init-date-time ((dt date-time) &rest args)
+  (defun init-utc (dt utc)
     (multiple-value-bind
-      (sec minute hour _date _month _year day-of-week daylight-p zone)
-      (decode-universal-time (get-universal-time) -9)
+      (sec minute hour date month year day-of-week daylight-p zone)
+      (decode-universal-time utc -9)
       (declare (ignorable daylight-p zone day-of-week))
-      (let* ((dt (make-instance 'date-time
-                                :year (if default? _year year)
-                                :month (if default? _month month)
-                                :date (if default? _date date)
-                                :hour (if default? hour 0)
-                                :minute (if default? minute 0)
-                                :sec (if default? sec 0)))
-             (utc (encode-universal-time (sec dt)
-                                         (minute dt)
-                                         (hour dt)
-                                         (date dt)
-                                         (month dt)
-                                         (year dt)
-                                         -9))
-             (day-of-week (mod (1+ (nth 6
-                                        (multiple-value-list
-                                          (decode-universal-time utc -9))))
-                               7)))
-        (setf (date-time-utc dt) utc
-              (date-time-day-of-week dt) day-of-week)
-        dt))))
+      (setf (date-time-utc dt) utc
+            (date-time-year dt) year
+            (date-time-month dt) month
+            (date-time-date dt) date
+            (date-time-hour dt) hour
+            (date-time-minute dt) minute
+            (date-time-sec dt) sec
+            (date-time-day-of-week dt) (mod (1+ day-of-week) 7)))
+    dt)
+  (defun init-now (dt) (init-utc dt (get-universal-time)))
+  (defun init-year-month-date (dt year month date)
+    (init-utc dt (encode-universal-time 0 0 0 date month year -9)))
+  (cond ((= (length args) 3)
+         (init-year-month-date dt (car args) (cadr args) (caddr args)))
+        ((= (length args) 1) (init-utc dt (car args)))
+        ((null args) (init-now dt))
+        (t (error "init-date-time: Illegal arguments '~A'." args))))
 
 (defmethod year ((dt date-time))
   (date-time-year dt))
@@ -78,3 +72,7 @@
     (cond ((= month 2) (if (leap-year? dt) 29 28))
           ((find month '(4 6 9 11)) 30)
           (t 31))))
+
+(defmethod next-day ((dt date-time))
+  (let ((next-day (make-instance 'date-time)))
+    (init-date-time next-day (+ (date-time-utc dt) (* 60 60 24)))))
