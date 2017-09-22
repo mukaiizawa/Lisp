c4d90e3b82d49f251e70fe96eac8531bfb9f3c96
mukaiizawa <mukaiizawa@yahoo.co.jp>
2017-09-22 13:34:50 +0900

    Fix required argument to option

--- bin/cal/xcal.lisp
+++ bin/cal/xcal.lisp
@@ -8,23 +8,25 @@
 (defparameter *calender-cycle* 28)
 
 (defparameter usage
-  (usage :title "xcal month date"
+  (usage :title "xcal [month date]"
          :desc '("Display a xcalendar.")))
 
 (defun month-add (month n)
   (1+ (mod (+ month -1 n) 12)))
 
 (defexe xcal ()
-  (when (/= (length args) 2) (funcall usage))
-  (let* ((ref-month (parse-int (car args)))
-         (ref-day (parse-int (cadr args)))
-         (dt (init-date-time (make-instance 'date-time))))
+  (unless (or (null args) (= (length args) 2)) (funcall usage))
+  (let* ((xdate? (not (null args)))
+         (today (init-date-time (make-instance 'date-time)))
+         (ref-month (if xdate? (parse-int (car args)) (month today)))
+         (ref-day (if xdate? (parse-int (cadr args)) (date today))))
     (format t "~10@A | Su Mo Tu We Th Fr Sa~%" #\Space)
     (format t "-----------+---------------------~%")
     (do* ((ref-date (init-date-time (make-instance 'date-time)
-                                    (year dt) ref-month ref-day))
-          (dt (before-date (init-date-time dt (year dt) ref-month 1)
-                           (date-time-day-of-week dt))
+                                    (year today) ref-month ref-day))
+          (dt (before-date (init-date-time (make-instance 'date-time)
+                                           (year today) ref-month 1)
+                           (date-time-day-of-week today))
               (after-date dt))
           (i 0 (if (or (plusp i) (date= dt ref-date)) (1+ i) 0)))
       ((= (month-add ref-month 3) (month dt)))
@@ -36,5 +38,7 @@
           (format t "~4@A-~2@A " (year it) (month it))
           (format t "~8@A" #\Space))
         (format t "~2@A | " (week-number dt)))
-      (format t "~2@A " (if (= (mod i *calender-cycle*) 1) "##" (date dt)))
+      (format t "~2@A " (cond ((and xdate? (= (mod i *calender-cycle*) 1)) "XX")
+                              ((date= dt today) "##")
+                              (t (date dt))))
       (when (= (1+ (day-of-week dt)) *week-day*) (format t "~%")))))
