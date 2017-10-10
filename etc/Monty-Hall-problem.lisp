76554f82f39780089daeeeacc336707b398e09f8
mukaiizawa <mukaiizawa@yahoo.co.jp>
2017-10-10 21:49:32 +0900

    Minor changes

--- etc/Monty-Hall-problem.lisp
+++ etc/Monty-Hall-problem.lisp
@@ -1,21 +1,21 @@
 #|
- | montie hole problem.
+ | monty hole problem.
  |#
 
 (defparameter *test-times* 100000)
 
 (defun do-game (change-choice?)
-  (let* ((master-choice (random 3))
+  (let* ((monty-choice (random 3))
          (user-choice (random 3))
          (openable (remove-if (lambda (x) 
-                                (or (= x master-choice) (= x user-choice)))
+                                (or (= x monty-choice) (= x user-choice)))
                               (loop for i from 0 to 2 collect i))))
     (when change-choice?
       (let ((open-choice (nth (random (length openable)) openable)))
         (setf user-choice (- (+ 0 1 2) user-choice open-choice))))
-    (= master-choice user-choice)))
+    (= monty-choice user-choice)))
 
-(defun montie-hole-problem ()
+(defun monty-hole-problem ()
   (let ((win-count-change 0)
         (win-count-not-change 0))
     (dotimes (i *test-times*)
@@ -25,4 +25,4 @@
             (/ win-count-change 1.0 *test-times*)
             (/ win-count-not-change 1.0 *test-times*))))
 
-(montie-hole-problem)
+(monty-hole-problem)
