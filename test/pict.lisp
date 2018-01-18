850e44e2f053822fd35635976d2318b4b1f4b14f
mukaiizawa <mukaiizawa@yahoo.co.jp>
2018-01-18 21:28:25 +0900

    Improved key parameter

--- test/pict.lisp
+++ test/pict.lisp
@@ -2,32 +2,32 @@
 (require :pict *module-pict*)
 
 #|
-PLATFORM: x86, ia64, amd64
-CPUS:     Single, Dual, Quad
-RAM:      128MB, 1GB, 4GB, 64GB
-HDD:      SCSI, IDE
-OS:       NT4, Win2K, WinXP, Win2K3
-IE:       4.0, 5.0, 5.5, 6.0
+    PLATFORM: X86, IA64, AMD64
+    CPUS:     SINGLE, DUAL, QUAD
+    RAM:      128MB, 1GB, 4GB, 64GB
+    HDD:      SCSI, IDE
+    OS:       NT4, WIN2K, WINXP, WIN2K3
+    IE:       4.0, 5.0, 5.5, 6.0
 
-# -------------------------------------------------------------------------
-# Separate submodels for hardware and software parameters make the hardware
-# configurations less variant lowering the cost of assembling the machines
-# -------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
+    # Separate submodels for hardware and software parameters make the hardware
+    # configurations less variant lowering the cost of assembling the machines
+    # -------------------------------------------------------------------------
 
-{ PLATFORM, CPUS, RAM, HDD } @ 2
-{ OS, IE } @ 2
+    { PLATFORM, CPUS, RAM, HDD } @ 2
+    { OS, IE } @ 2
 
-# -------------------------------------------------------------------------
-# Constraints may cross submodel boundaries; here OS depends on PLATFORM
-# -------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
+    # Constraints may cross submodel boundaries; here OS depends on PLATFORM
+    # -------------------------------------------------------------------------
 
-IF [PLATFORM] in {"ia64", "amd64"} THEN [OS] in {"WinXP", "Win2K3"};
+    IF [PLATFORM] IN {"IA64", "AMD64"} THEN [OS] IN {"WINXP", "WIN2K3"};
 
-# ------------------------------------------------------------------------
-# Constraints on parameters in the same submodel are also fine
-# ------------------------------------------------------------------------
+    # ------------------------------------------------------------------------
+    # Constraints on parameters in the same submodel are also fine
+    # ------------------------------------------------------------------------
 
-IF [PLATFORM] = "x86" THEN [RAM] <> "64GB";
+    IF [PLATFORM] = "X86" THEN [RAM] <> "64GB";
 |#
 
 (defparameter factors
