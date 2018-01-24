7d0397a043dc5a8568fa45f58c31387bdcf69039
mukaiizawa <mukaiizawa@yahoo.co.jp>
2018-01-24 11:09:12 +0900

    Upgrade grammer

--- lib/pict.lisp
+++ lib/pict.lisp
@@ -1,61 +1,134 @@
-#|
- | pict wrapper
- |#
+; pict wrapper
 
-(require :stdlib *module-stdlib*)
 (provide :pict)
 
-(defun parse-condition (tokens)
-  (with-output-to-string (out)
-    (let ((ope (car tokens)))
-      (cond ((or (eq ope 'and) (eq ope 'or))
-             (if (< (length tokens) 3) (error "illegal ~A clause" ope))
-             (format out " (")
-             (reduce (lambda (x y)
-                       (format out " ~A ~A ~A"
-                               (parse-condition x) ope (parse-condition y)))
-                     (rest tokens))
-             (format out ")"))
-            ((eq ope 'in)
-             (if (< (length tokens) 3) (error "illegal in clause"))
-             (format out " [~A] in {~{\"~A\"~^, ~}}"
-                     (second tokens) (cddr tokens)))
-            ((or (eq ope '=) (eq ope '/=))
-             (if (/= (length tokens) 3) (error "illegal ~A clause" ope))
-             (format out " [~A] ~A \"~A\"" (second tokens) ope (third tokens)))
-            ((or (eq ope '<) (eq ope '<=) (eq ope '>) (eq ope '>=))
-             (if (/= (length tokens) 3) (error "illegal ~A clause" ope))
-             (format out " [~A] ~A ~A" (second tokens) ope (third tokens)))
-            (t (error "illegal operator ~A" ope))))
-    out))
+; original constraint grammer
+; <constraints> ::= <constraint> [<constraint>]
+; <constraint> ::= <predicate>
+;                    | IF <predicate> THEN <predicate> [ELSE <predicate>];
+; <predicate> ::= { ( <predicate> )
+;                    | NOT <predicate>
+;                    | <param_name> { = | <> | > | >= | < | <= }
+;                                   { <term_param>> | <value>}
+;                    | <param_name> IN '{' <value> [, <value>] ... '}'
+;                    | <param_name> LIKE <regex> }
+;                 [ { AND | OR } <predicate> ]
+; <param_name> ::= '[' <identifier> ']'
+; <value> ::= { <param_name> | " <identifier> " | <number> }
+; <identifier> -- whatever is typically regarded as a identifier
+; <number> -- whatever is typically regarded as a number
+; <regex> -- string with embedded special characters
+;              *: a series of characters of any length (can be zero)
+;              ?: any one character
 
-(defun parse-clause (tokens)
-  (with-output-to-string (out)
-    (if (/= (length tokens) 3) (error "illegal clause."))
-    (if (not (eq (first tokens) 'if)) (error "if is exprected."))
-    (format out "IF ~A THEN ~A;"
-            (parse-condition (second tokens))
-            (parse-condition (third tokens)))
-    out))
+; pict DSL grammer
+; (pict <params> &key <submodels> <constraints>)
+; <params> ::= (<param> [<param>] ...)
+; <param> ::= (<symbol> <param_value> [<param_value>] ...)
+; <param_value> ::= { <alias> | <value> }
+; <alias> ::= ( <value> [<value>] ...)
+; <submodels> ::= (<submodel> [<submodel>] ...)
+; <submodel> ::= (<number> <symbol> [<symbol>] ...)
+; <constraints> ::= (<constraint> [<constraint>] ...)
+; <constraint> ::= { <predicate> | (if <predicate> <predicate> [<predicate>]) }
+; <predicate> ::= {
+;   (not <predicate>)
+;   | (in <symbol> <value>>)
+;   | (like <symbol> <regex>)
+;   | ({ and | or } <predicate> [<predicate>] ...)
+;   | ({ = | /= | < | <= | > | >= } <symbol> <value>)
+; }
+; <value> ::= { <symbol> | <string> | <number> }
+; <symbol> -- a symbol
+; <string> -- a string
+; <number> -- a number
 
-; (pict <factor-list> [<submodel-list>] [<condition-list>])
-; <factor-list> ::= (factor-name factor [factor] ...)
-; <submodel-list> ::= (submodel [submodel] ...)
-; <submodel> ::= (factor-name factor-name [factor-name] ...)
-; <constraint-list> ::= (<constraint> [<constraint>] ...)
-; <constraint> ::= <clause> [<clasuse>] ...
-; <clause> ::= (if <condition> <condition>)
-; <condition> ::= ({ and | or } <condition> [<condition>] ...)
-;                 | (in factor [factor] ...)
-;                 | ({ = | /= | < | <= | > | >= } factor factor)
-(defun pict (factor-list &optional model-list condition-list
-                         &key (output "pict.out") invoke?)
-  (with-open-file (pict-in output :direction :output :if-exists :supersede)
-    (format pict-in "# following rules are generated automatically.~%")
-    (dolist (factor factor-list)
-      (format pict-in "~A: ~{~A~^, ~}~%" (car factor) (cdr factor)))
-    (dolist (model model-list)
-      (format pict-in "{ ~{~A~^, ~} } @ ~A~%" model (length model)))
-    (dolist (condition condition-list)
-      (format pict-in "~A~%" (parse-clause condition))))
-  (if invoke? (call "pict" (list output))))
+(defun map-op (op)
+  (case op
+    ((< <= > >= =) op)
+    ((/=) '<>)))
+
+(defun pict (params &key submodels constraints (file "pict.arg"))
+  (with-open-file (out file :direction :output :if-exists :supersede)
+    (let (param-names)
+      (labels
+        ((parse-params ()
+           (dolist (param params)
+             (let ((param-name (car param)) (param-values (cdr param)))
+               (if (not (atom param-name))
+                 (error "parameter name must be symbol `~A'." param-name))
+               (push param-name param-names)
+               (format out "~A: ~{~A~^ ,~}~%"
+                       param-name
+                       (mapcar (lambda (v)
+                                 (if (atom v) v (format nil "~{~A~^ | ~}" v)))
+                               param-values)))))
+         (parse-submodels ()
+           (dolist (submodel submodels)
+             (let ((order (car submodel)) (submodel-members (cdr submodel)))
+               (if (not (numberp order))
+                 (error "submodel order `~A' must be number." order))
+               (dolist (param-name submodel-members)
+                 (if (not (find param-name param-names))
+                   (error "submodel member `~A' must be in ~A."
+                          param-name param-names)))
+               (format out "{ ~{~A~^, ~} } @ ~A~%" submodel-members order))))
+         (parse-conditions ()
+           (dolist (constraint constraints)
+             (if (not (eq (car constraint) 'if))
+               (format out "~A" (parse-predicate constraint))
+               (let ((predicate-num (length (cdr constraint))))
+                 (if (not(or (= predicate-num 2) (= predicate-num 3)))
+                   (error "illegal arguments ~A." constraint))
+                 (format out "IF ~A THEN ~A"
+                         (parse-predicate (cadr constraint))
+                         (parse-predicate (caddr constraint)))
+                 (if (= predicate-num 3)
+                   (format out " ELSE ~A"
+                           (parse-predicate (cadddr constraint))))))
+             (format out ";~%")))
+         (parse-predicate (predicate)
+           (with-output-to-string (s)
+             (let* ((op (car predicate))
+                    (args (cdr predicate))
+                    (argc (length args)))
+               (if (not (symbolp op)) (error "operator `~A' must be symbol" op))
+               (case op
+                 ((not)
+                  (if (/= argc 1) (error "illegal arguments ~A" args))
+                  (format s "not ( ~A )" (parse-predicate (car args))))
+                 ((in)
+                  (if (not (find (car args) param-names))
+                    (error "undefined parameter name `~A'." (car args)))
+                  (format s "[~A] in {~{~A~^, ~}}"
+                          (car args)
+                          (mapcar #'parse-predicate-value (cdr args))))
+                 ((like) (error "like not supported."))
+                 ((and or)
+                  (if (< argc 2) (error "illegal ~A arguments" op))
+                  (reduce (lambda (x y)
+                            (format s "( ~A ~A ~A )"
+                                    (parse-predicate x) op
+                                    (parse-predicate y)))
+                          args))
+                 ((= /= < <= > >=)
+                  (if (/= argc 2) (error "illegal arguments ~A" args))
+                  (if (not (find (car args) param-names))
+                    (error "illegal parameter name `~A'." (car args)))
+                  (format s "[~A] ~A ~A"
+                          (car args)
+                          (map-op op)
+                          (parse-predicate-value (cadr args))))))
+             s))
+         (parse-predicate-value (val)
+           (cond ((numberp val) val)
+                 ((stringp val) (format nil "\"~A\"" val))
+                 ((symbolp val)
+                  (if (find val param-names)
+                    (format nil "[~A]" val)
+                    (format nil "\"~A\"" val)))
+                 (t (error "illegal predicate value `~A'." val)))))
+        (format out "# following rules are generated automatically.~%")
+        (parse-params)
+        (parse-submodels)
+        (parse-conditions)))))
--- test/pict.lisp
+++ test/pict.lisp
@@ -1,20 +1,20 @@
-(require :stdlib *module-stdlib*)
 (require :pict *module-pict*)
 
 #|
     PLATFORM: X86, IA64, AMD64
-    CPUS:     SINGLE, DUAL, QUAD
-    RAM:      128MB, 1GB, 4GB, 64GB
-    HDD:      SCSI, IDE
-    OS:       NT4, WIN2K, WINXP, WIN2K3
-    IE:       4.0, 5.0, 5.5, 6.0
+    CPU: i3, i5, i7
+    RAM1: 128MB, 1GB, 4GB, 64GB
+    RAM2: 128MB, 1GB, 4GB, 64GB
+    HDD: SCSI, IDE
+    OS: xp, 7, 8, 8.1, 10
+    IE: 4.0, 5.0, 5.5, 6.0
 
     # -------------------------------------------------------------------------
     # Separate submodels for hardware and software parameters make the hardware
     # configurations less variant lowering the cost of assembling the machines
     # -------------------------------------------------------------------------
 
-    { PLATFORM, CPUS, RAM, HDD } @ 2
+    { PLATFORM, CPU, HDD } @ 2
     { OS, IE } @ 2
 
     # -------------------------------------------------------------------------
@@ -27,21 +27,27 @@
     # Constraints on parameters in the same submodel are also fine
     # ------------------------------------------------------------------------
 
-    IF [PLATFORM] = "X86" THEN [RAM] <> "64GB";
+    IF [PLATFORM] = "X86" THEN [RAM1] <> "64GB";
 |#
 
-(defparameter factors
-  '(("PLATFORM" "X86" "IA64" "AMD64")
-    ("CPUS" "SINGLE" "DUAL" "QUAD")
-    ("RAM" "128MB" "1GB" "4GB" "64GB")
-    ("HDD" "SCSI" "IDE")
-    ("OS" "XP" "WIN2K" "WINXP" "WIN2K3")
-    ("IE" "4.0" "5.0" "5.5" "6.0")))
+(defparameter RAM '("128MB" "1GB" "4GB" "64GB"))
+(defparameter params
+  `((PLATFORM "X86" "IA64" "AMD64")
+    (CPU "i3" "i5" "i7")
+    (RAM1 ,@RAM)
+    (RAM2 ,@RAM)
+    (HDD "SCSI" "IDE")
+    (OS "xp" "7" "8" "8.1" "10")
+    (IE 4.0 5.0 5.5 6.0)))
 
-(defparameter models '(("OS" "IE")))
+(defparameter submodels '((2 PLATFORM CPU HDD)
+                          (2 OS IE)))
 
-(defparameter conditions
-  '((if (or (in "PLATFORM" "X86" "IA64") (and (= "RAM" "128MB") (> "IE" "4.0")))
-      (= "HDD" "IDE"))))
+(defparameter constraints
+  '((if (or (in PLATFORM "X86" "IA64")
+            (and (= RAM1 "128MB") (> IE 4.0)))
+      (= HDD "IDE")
+      (= HDD "SCSI"))
+    (/= RAM1 RAM2)))
 
-(pict factors models conditions)
+(pict params :submodels submodels :constraints constraints)
