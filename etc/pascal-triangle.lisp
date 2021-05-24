82782e8a0d35581db7d7aa2be30af94bfd586313
Shintaro Mukai <mukaiizawa@gmail.com>
2021-05-24 16:31:14 +0900

    Minor change

--- etc/pascal-triangle.lisp
+++ etc/pascal-triangle.lisp
@@ -1,11 +1,10 @@
 (require :stdlib *module-stdlib*)
 
 (defun pascal (i j)
-  (when (<= j i )
-    (if (or (= j 0) (= i j))
-      1
-      (+ (pascal (1- i) (1- j))
-         (pascal (1- i) j)))))
+  (if (or (= j 0) (= i j))
+    1
+    (+ (pascal (1- i) (1- j))
+       (pascal (1- i) j))))
 
 (defun pascal-line (i)
   (mapcar (lambda (j)
